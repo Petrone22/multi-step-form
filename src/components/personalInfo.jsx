6b558ffbdf103a36b1eb12ef139e68efdb78e7fa
Petrone22 <the1rashid@gmai.com>
2023-02-16 15:58:17 +0200

    committed changes

--- src/components/personalInfo.jsx
+++ src/components/personalInfo.jsx
@@ -1,21 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 
-const PersonalInfo = ({ name, email, phone }) => {
+const PersonalInfo = ({ setName, setEmail, setPhone }) => {
   return (
     <div className="personal-info-container">
       <h1>Personal info</h1>
       <p>Please provide your name, email address, and phone number.</p>
       <div className="input">
         <div className="name-div">Name</div>
-        <input type="text" value={name} placeholder="e.g. Stephen King" />
+        <input
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder="e.g. Stephen King"
+        />
         <div className="email-div">Email Adress</div>
         <input
           type="text"
-          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
           placeholder="e.g. stephenking@lorem.com"
         />
         <div className="number-div">Phone number</div>
-        <input type="text" value={phone} placeholder="e.g. +1 234 567 890" />
+        <input
+          type="text"
+          onChange={(e) => {
+            setPhone(e.target.value);
+          }}
+          placeholder="e.g. +1 234 567 890"
+        />
       </div>
     </div>
   );
--- src/components/plan.jsx
+++ src/components/plan.jsx
@@ -1,22 +1,38 @@
 import React from "react";
 
-const Plan = () => {
+const Plan = ({ plan, setPlan }) => {
+  console.log(plan);
   return (
     <div className="plan-selection">
       <h1>Select your plan</h1>
       <p>You have the option of monthly or yearly billing.</p>
       <div className="plan-capsule">
-        <div className="plan-div">
+        <div
+          className="plan-div"
+          onClick={() => {
+            setPlan(0);
+          }}
+        >
           <img id="img1" src="" alt="" />
           <div className="plan-title">Arcade</div>
           <div className="plan-price">$9/mo</div>
         </div>
-        <div className="plan-div">
+        <div
+          className="plan-div"
+          onClick={() => {
+            setPlan(1);
+          }}
+        >
           <img id="img2" src="" alt="" />
           <div className="plan-title">Advanced</div>
           <div className="plan-price">$12/mo</div>
         </div>
-        <div className="plan-div">
+        <div
+          className="plan-div"
+          onClick={() => {
+            setPlan(2);
+          }}
+        >
           <img id="img3" src="" alt="" />
           <div className="plan-title">Pro</div>
           <div className="plan-price">$15/mo</div>
